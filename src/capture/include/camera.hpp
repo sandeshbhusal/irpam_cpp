f3fae8b1bc35ce6ab8da5758856bb1568099bc6e
sandeshbhusal <mail.sandeshbhusal@gmail.com>
2025-02-13 13:41:33 -0700

    Enumerating input video devices works now.

--- src/capture/include/camera.hpp
+++ src/capture/include/camera.hpp
@@ -7,14 +7,17 @@
 #include <vector>
 #include <sys/ioctl.h>
 #include <linux/videodev2.h>
+#include <errno.h>
 #include <fcntl.h>
 
 #include "libv4l2.h"
+#include <string.h>
 
 /**
  * @brief PictureFormat structure to hold the format of the camera.
  */
-struct PictureFormat {
+struct PictureFormat
+{
     uint32_t fourcc;
     int width;
     int height;
@@ -28,45 +31,42 @@ class Camera
 {
 private:
     std::string camera_path;
-    std::string card_name;
-    std::string driver_name;
-    bool is_capture_device = false;
-    std::vector<PictureFormat> formats;
+    v4l2_capability cap{}; // Initialize to zero
+    int fd = -1;
 
 public:
+    ~Camera()
+    {
+        if (this->fd > -1)
+            close(this->fd);
+    }
+
     explicit Camera(const std::string &camera_path)
         : camera_path(camera_path)
     {
-        // Ensure the camera exists.
         if (!std::filesystem::exists(camera_path))
             throw std::runtime_error("Camera path does not exist: " + camera_path);
-        
-        // Query the device for its formats.
-        v4l2_capability cap;
-        int fd = v4l2_open(camera_path.c_str(), O_RDWR);
-        if (fd < 0)
-            throw std::runtime_error("Failed to open camera: " + camera_path);
-        
-        if (v4l2_ioctl(fd, VIDIOC_QUERYCAP, &cap) < 0)
-            throw std::runtime_error("Failed to query camera capabilities: " + camera_path); 
 
-        // Ensure this is a video capture device. If not, mark it as a video output device.
-        this -> is_capture_device = (cap.capabilities & V4L2_CAP_VIDEO_CAPTURE) != 0;
-        this -> card_name = std::string(reinterpret_cast<char*>(cap.card), sizeof(cap.card));
-        this -> driver_name = std::string(reinterpret_cast<char*>(cap.driver), sizeof(cap.driver));
+        this->fd = v4l2_open(camera_path.c_str(), O_RDWR);
+        if (this->fd < 0)
+            throw std::runtime_error("Failed to open camera: " + std::string(strerror(errno)));
 
-        close(fd);
-        
-        // If this is an output device, we are done at this point.        
-        if (!this->is_capture_device) return;
+        if (v4l2_ioctl(fd, VIDIOC_QUERYCAP, &this->cap) < 0)
+            throw std::runtime_error("Failed to query camera caps: " + std::string(strerror(errno)));
     }
 
-    bool isCaptureDevice() {
-        return false;
+    bool isCaptureDevice() const
+    {
+        int input = 0;
+        if (v4l2_ioctl(fd, VIDIOC_G_INPUT, &input) < 0)
+            return false;
+        
+        return true;
     }
 
-    std::string getCameraPath() const {
-        return camera_path;
+    const std::string getCameraPath() const
+    {
+        return this->camera_path;
     }
 };
 
--- tests/camera.cpp
+++ tests/camera.cpp
@@ -3,15 +3,19 @@
 
 TEST(checkTest, getcapturedevices) {
     std::vector<std::string> cameras = availableCameras();
-
     std::vector<std::string> captureDevices;
+    
     for (const auto& camera : cameras) {
-        Camera cam(camera);
-        if (cam.isCaptureDevice()) {
-            captureDevices.push_back(camera);
-            std::cerr << "Camera Path: " << cam.getCameraPath() << std::endl;
+        try {
+            Camera cam(camera);
+            
+            if (cam.isCaptureDevice()) {
+                captureDevices.push_back(camera);
+            }
+        } catch (const std::exception& e) {
+            std::cerr << "Error with " << camera << ": " << e.what() << std::endl;
         }
     }
-
+    
     EXPECT_GE(captureDevices.size(), 1) << "At least one camera should be available";
 }
