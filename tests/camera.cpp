e833d4972afb5686a2816fcf67340fc099b00ddb
sandeshbhusal <mail.sandeshbhusal@gmail.com>
2025-02-13 13:19:50 -0700

    Added camera listing, format and cap enumeration do not work

--- tests/camera.cpp
+++ tests/camera.cpp
@@ -1,6 +1,17 @@
 #include <gtest/gtest.h>
 #include "camera.hpp"
 
-TEST(checkTest, first) {
+TEST(checkTest, getcapturedevices) {
+    std::vector<std::string> cameras = availableCameras();
 
+    std::vector<std::string> captureDevices;
+    for (const auto& camera : cameras) {
+        Camera cam(camera);
+        if (cam.isCaptureDevice()) {
+            captureDevices.push_back(camera);
+            std::cerr << "Camera Path: " << cam.getCameraPath() << std::endl;
+        }
+    }
+
+    EXPECT_GE(captureDevices.size(), 1) << "At least one camera should be available";
 }
