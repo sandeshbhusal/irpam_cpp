02f84fc87e1b1883a9fe43a3450cd1535b7a1af1
sandeshbhusal <mail.sandeshbhusal@gmail.com>
2025-02-13 20:57:31 -0700

    Exploring model loading; going to push this for today

--- src/recognition/recognition.cpp
+++ src/recognition/recognition.cpp
@@ -1,6 +1,11 @@
 #include "include/recognition.hpp"
+#include <stdexcept>
 
 extern "C" void recognize_image(void *imageData, int width, int height)
 {
     ncnn::Net network;
+    // TODO: Here, the embedded resource is to be read.
+    if (network.load_model((const char*) nullptr) < 1) {
+        throw std::runtime_error("Failed to load the model from the file.");
+    }
 }
