9148bb57d0f65ffe61a50f05f550a9eb16250f5f
sandeshbhusal <mail.sandeshbhusal@gmail.com>
2025-02-13 20:50:26 -0700

    chore: Added ncnn as a dependency

---
+++ src/recognition/include/recognition.hpp
@@ -0,0 +1,6 @@
+#ifndef RECOGNITION_H
+#define RECOGNITION_H
+
+#include "net.h"
+
+#endif
---
+++ src/recognition/recognition.cpp
@@ -0,0 +1,6 @@
+#include "include/recognition.hpp"
+
+extern "C" void recognize_image(void *imageData, int width, int height)
+{
+    ncnn::Net network;
+}
